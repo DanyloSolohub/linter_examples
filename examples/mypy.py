8ee8acf1fc8414a5380d890e3c546e25b8467b15
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2023-01-10 01:49:39 +0200

    added examples with mypy

---
+++ examples/mypy.py
@@ -0,0 +1,12 @@
+"""
+mypy:
+
+    def example_function(first_parameter: list, second_parameter: set, third_parameter: int):
+        first_parameter = first_parameter + second_parameter + third_parameter
+        return first_parameter
+Result:
+
+    examples/mypy.py:1: error: Function is missing a return type annotation
+    examples/mypy.py:2: error: Unsupported operand types for + ("List[Any]" and "Set[Any]")
+    examples/mypy.py:2: error: Unsupported operand types for + ("List[Any]" and "int")
+"""
