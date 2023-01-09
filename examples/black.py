d35b99e26abca80e4caa26c7be8993f239fe1b56
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2023-01-10 01:45:39 +0200

    added examples with black

---
+++ examples/black.py
@@ -0,0 +1,15 @@
+"""
+Before:
+
+    def example_func(
+            some_variable, another_variable):
+        result  = some_variable + \
+                  another_variable
+        return                 result
+After:
+
+    def example_func(some_variable, another_variable):
+    result = some_variable + another_variable
+    return result
+
+"""
