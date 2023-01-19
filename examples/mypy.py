67c495ab253d9e2037bad0202b5b01d6e3f55da8
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2023-01-19 14:35:41 +0200

    [DS-69] added ruff

--- examples/mypy.py
+++ examples/mypy.py
@@ -1,12 +1,12 @@
-"""
-Mypy:
+# """
+# Mypy:
 
-    def example_function(first_parameter: list, second_parameter: set, third_parameter: int):
-        first_parameter = first_parameter + second_parameter + third_parameter
-        return first_parameter
-Result:
+def example_function(first_parameter: list, second_parameter: set, third_parameter: int):
+    first_parameter = first_parameter + second_parameter + third_parameter
+    return first_parameter
+# Result:
 
-    examples/mypy.py:1: error: Function is missing a return type annotation
-    examples/mypy.py:2: error: Unsupported operand types for + ("List[Any]" and "Set[Any]")
-    examples/mypy.py:2: error: Unsupported operand types for + ("List[Any]" and "int")
-"""
+#     examples/mypy.py:1: error: Function is missing a return type annotation
+#     examples/mypy.py:2: error: Unsupported operand types for + ("List[Any]" and "Set[Any]")
+#     examples/mypy.py:2: error: Unsupported operand types for + ("List[Any]" and "int")
+# """
